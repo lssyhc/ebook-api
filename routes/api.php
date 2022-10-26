9e427318fd410cc4cc67e9cc4574ff062c63e30d
Cahyo Susilo <cahyosusilo775@gmail.com>
2022-10-26 10:10:49 +0700

    routing + controller + model + migration

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HelloController;
+use App\Http\Controllers\SiswaController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +16,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+// Halo
+// Route::get('halo', function () {
+//     return ['me' => 'Guanteng'];
+// });
+
+// Siswa
+// Route::get('/api/siswa', [SiswaController::class, 'index']);
+// Route::get('/api/siswa/{id}', [SiswaController::class, 'show']);
+// Route::post('/api/siswa', [SiswaController::class, 'store']);
+// Route::put('/api/siswa/{id}', [SiswaController::class, 'update']);
+// Route::delete('/api/siswa/{id}', [SiswaController::class, 'destroy']);
+
+// Resource
+Route::resource('halo', HelloController::class);
+Route::resource('siswa', SiswaController::class);
